42fee12c64b5ba4e15156a7e8e758350f03fce7d
“Kathleen <kgmcdonnell@mail.usf.edu>
2023-02-28 21:04:16 -0500

    added products to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts ".......Seeding....."
+Product.create({ name: "By the Fireplace Replica Perfume", price: 160.00, image_url: "https://media.kohlsimg.com/is/image/kohls/5142337?wid=1200&hei=1200&op_sharpen=1", description: "A warm scent combines orange flower, clove oil, and chestnut atop a comforting vanilla fragrance to re-create the signature warmth and coziness of a crackling fire" })
+
+Product.create({ name: "Forever Skin Glow Dior Foundation", price: 55.00, image_url: "https://eco-beauty.dior.com/dw/image/v2/BDGF_PRD/on/demandware.static/-/Sites-master_dior/default/dwb7963a44/assets/Y0996398/Y0996398_C023650020_E01_ZHC.jpg?sw=870&sh=580&sm=fit&imwidth=870", description: "Dior Forever Skin Glow is the radiant foundation by Dior that gives high perfection to the complexion with 24h wear." })
+
+Product.create({ name: "Dyson Airwrap Styler", price: 599.99, image_url: "https://dyson-h.assetsadobe2.com/is/image/content/dam/dyson/images/products/hero/400714-01.png?$responsive$&cropPathE=desktop&fit=stretch,1&wid=960", description: "The only multi-styler to curl, shape, and hide flyaways using the Coanda effect. Not extreme heat." })
+
+Product.create({ name: "Purity One-Step Facial Cleanser", price: 41.00, image_url: "https://media.ulta.com/i/ulta/2583945?w=720&fmt=webp", description: "Deep-cleans pores, eliminates makeup buildup, and lightly hydrates and tones skin" })
+
+Product.create({ name: "Charlotte Tilbury Matte Revolution Lipstick - Pillow Talk", price: 35.00, image_url: "https://images.ctfassets.net/wlke2cbybljx/6QLItVWaR3INrE5B8MMObK/79854007d3f3ed8a40a1e4d7e0c21e12/pillow-talk-packshot.png?w=500&h=500&fit=fill&fm=jpg&bg=", description: "Matte Revolution in Pillow Talk enhances the natural hue of your lips so is unique to your skin tone and natural lip color." })
+puts "Done!"

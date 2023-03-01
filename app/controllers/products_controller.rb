2e8d9936e5032aeba2ff5bdfbcee7b44dd7c06d1
“Kathleen <kgmcdonnell@mail.usf.edu>
2023-02-28 21:19:18 -0500

    created routes to display individual products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,24 @@ class ProductsController < ApplicationController
   def all_products
     render json: Product.all.as_json
   end
+
+  def perfume
+    render json: Product.find_by(id: 1)
+  end
+
+  def hair_tools
+    render json: Product.find_by(id: 3)
+  end
+
+  def foundation
+    render json: Product.find_by(id: 2)
+  end
+
+  def cleanser
+    render json: Product.find_by(id: 4)
+  end
+
+  def lip_products
+    render json: Product.find_by(id: 5)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,14 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get "/products", controller: "products", action: "all_products"
+
+  get "/perfume", controller: "products", action: "perfume"
+
+  get "/hair_tools", controller: "products", action: "hair_tools"
+
+  get "/foundation", controller: "products", action: "foundation"
+
+  get "/cleanser", controller: "products", action: "cleanser"
+
+  get "/lipstick", controller: "products", action: "lip_products"
 end

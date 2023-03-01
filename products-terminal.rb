342127a04a23d0d4d5b324fec11e99a9d45a6964
“Kathleen <kgmcdonnell@mail.usf.edu>
2023-02-28 22:03:53 -0500

    added a feature to products_terminal.rb to display product information based on user input

--- products-terminal.rb
+++ products-terminal.rb
@@ -2,9 +2,29 @@ require "http"
 
 response = HTTP.get("http://localhost:3000/products")
 data = response.parse(:json)
-#displaying product names
-puts "Products:"
-puts data[0]["name"]
-puts data[1]["name"]
-puts data[2]["name"]
-puts data[3]["name"]
+
+while true
+  #displaying producta
+  puts "----------------------------------"
+  puts "Products:"
+  puts "1. #{data[0]["name"]}"
+  puts "2. #{data[1]["name"]}"
+  puts "3. #{data[2]["name"]}"
+  puts "4. #{data[3]["name"]}"
+  puts "----------------------------------"
+
+  #ask the user what product they would like more information on
+  puts "Would you like to see more information about a product? (Y/N)"
+  response = gets.chomp
+  if response.downcase == "y"
+    puts "Enter Product Number: "
+    product_id = gets.chomp
+    puts data[product_id.to_i - 1]
+    sleep(30)
+  elsif response.downcase == "n"
+    puts "Goodbye!"
+    break
+  else
+    puts "Invalid response. Try again"
+  end
+end

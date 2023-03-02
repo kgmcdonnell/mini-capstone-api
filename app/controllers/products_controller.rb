34c2784189d8c9ef3ae3e99e032b94e2fda211fe
“Kathleen <kgmcdonnell@mail.usf.edu>
2023-03-01 21:53:16 -0500

    created a create action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,15 @@ class ProductsController < ApplicationController
     product = Product.find_by(id: products_id)
     render json: product.as_json
   end
+
+  def create
+    product = Product.new(
+      name: "Satin Ultra Petite Scrunchies",
+      price: 10.00,
+      image_url: "https://media.ulta.com/i/ulta/2607115?w=720&fmt=webp",
+      description: "Crafted with eco-friendly satin, these accessories are gentle on your strands and help reduce damage to your hair.",
+    )
+    product.save
+    render json: product
+  end
 end

a06aeccdf80eda39f055a85d06e98ff618711245
“Kathleen <kgmcdonnell@mail.usf.edu>
2023-03-02 20:25:34 -0500

    added update action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,4 +19,16 @@ class ProductsController < ApplicationController
     product.save
     render json: product.as_json
   end
+
+  def update
+    product_id = params[:id]
+    product = Product.find(product_id)
+    product.update(
+      name: params[:name] || product.name,
+      price: params[:price] || product.price,
+      image_url: params[:image_url] || product.image_url,
+      description: params[:description] || product.description,
+    )
+    render json: product.as_json
+  end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -24,4 +24,11 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
       post "/products.json", params: { name: "Dermalogica Circular Hydration Serum", price: 64.00, image_url: "https://media.ulta.com/i/ulta/2591108?w=720&fmt=webp", description: "This long lasting serum immediately floods skin with hydration and helps prevent future hydration evaporation." }
     end
   end
+
+  test "update" do
+    patch "/products/#{Product.first.id}.json", params: { price: 200 }
+    assert_response 200
+    data = JSON.parse(response.body)
+    assert_equal 200, data["price"]
+  end
 end

bf79544a06a380704fe34f9ccebb16498671b70f
“Kathleen <kgmcdonnell@mail.usf.edu>
2023-03-06 19:56:44 -0500

    added validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+
+  validates :price, numericality: { greater_than: 0 }
+
+  validates :description, length: { minimum: 10, maximum: 500 }
 end

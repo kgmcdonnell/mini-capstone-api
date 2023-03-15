1bb09180328ca3e243fbaba31b940177e991b2e1
“Kathleen <kgmcdonnell@mail.usf.edu>
2023-03-15 19:37:27 -0400

    added authorization to the orders controller index and create action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,35 +1,40 @@
 class OrdersController < ApplicationController
-  def create
-    subtotal = params[:quantity] * Product.find_by(id: params[:product_id]).price
-    tax = subtotal * 0.09
-    total = subtotal + tax
-    @order = Order.new(
-      user_id: current_user.id,
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      subtotal: subtotal,
-      tax: tax,
-      total: total,
-    )
-    if @order.save
-      render json: { message: "order successfully created!" }, status: 200
+  def index
+    if current_user
+      @orders = current_user.orders
+      render template: "orders/index"
     else
-      render json: { errors: order.errors.full_messages }, status: :bad_request
+      render json: [], status: :unauthorized
+    end
+  end
+
+  def create
+    if current_user
+      subtotal = params[:quantity].to_i * Product.find_by(id: params[:product_id]).price
+      tax = subtotal * 0.09
+      total = subtotal + tax
+      @order = Order.new(
+        user_id: current_user.id,
+        product_id: params[:product_id],
+        quantity: params[:quantity],
+        subtotal: subtotal,
+        tax: tax,
+        total: total,
+      )
+      if @order.save
+        render template: "orders/show"
+      else
+        render json: { errors: @order.errors.full_messages }, status: 422
+      end
+    else
+      render json: [], status: :unauthorized
     end
   end
 
   def show
     order_id = params[:id]
-    @order = Order.where(user_id: current_user).find_by(id: order_id)
-    if @order
-      render template: "orders/show"
-    else
-      render json: { errors: "order does not exist" }, status: :bad_request
-    end
-  end
+    @order = current_user.orders.find_by(id: order_id)
 
-  def index
-    @orders = current_user.orders
-    render template: "orders/index"
+    render template: "orders/show"
   end
 end

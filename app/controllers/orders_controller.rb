c74b5f7b0fca1609b568a71ed1b2eb16460d835a
“Kathleen <kgmcdonnell@mail.usf.edu>
2023-03-15 20:21:07 -0400

    added authenticate_admin method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,40 +1,34 @@
 class OrdersController < ApplicationController
+  #check to make sure a user is logged in
+  before_action :authenticate_user
+
   def index
-    if current_user
-      @orders = current_user.orders
-      render template: "orders/index"
-    else
-      render json: [], status: :unauthorized
-    end
+    @orders = current_user.orders
+    render template: "orders/index"
   end
 
   def create
-    if current_user
-      subtotal = params[:quantity].to_i * Product.find_by(id: params[:product_id]).price
-      tax = subtotal * 0.09
-      total = subtotal + tax
-      @order = Order.new(
-        user_id: current_user.id,
-        product_id: params[:product_id],
-        quantity: params[:quantity],
-        subtotal: subtotal,
-        tax: tax,
-        total: total,
-      )
-      if @order.save
-        render template: "orders/show"
-      else
-        render json: { errors: @order.errors.full_messages }, status: 422
-      end
+    subtotal = params[:quantity].to_i * Product.find_by(id: params[:product_id]).price
+    tax = subtotal * 0.09
+    total = subtotal + tax
+    @order = Order.new(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: subtotal,
+      tax: tax,
+      total: total,
+    )
+    if @order.save
+      render template: "orders/show"
     else
-      render json: [], status: :unauthorized
+      render json: { errors: @order.errors.full_messages }, status: 422
     end
   end
 
   def show
     order_id = params[:id]
     @order = current_user.orders.find_by(id: order_id)
-
     render template: "orders/show"
   end
 end
